8e447f8d2bcd6574dc45b9ff8e9aad2ea7e4d150
ecarlotti <edson@leadcomm.com.br>
2022-01-13 18:04:37 -0300

    Conversion to ecarlotti package

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 /* eslint-disable */
 Package.describe({
-  name: 'ermlab:office365-oauth',
-  version: '0.2.0',
+  name: 'ecarlotti:office365-oauth',
+  version: '0.2.1',
   summary: 'Microsoft Office 365 OAuth flow',
-  git: 'https://github.com/Ermlab/meteor-office365-oauth.git',
+  git: 'https://github.com/ecarlotti/meteor-office365-oauth.git',
   documentation: 'README.md',
 });
 
@@ -11,9 +11,9 @@ Package.onUse(function(api) {
   api.versionsFrom('1.5.1');
 
   api.use('ecmascript');
-  api.use('oauth2', ['client', 'server']);
-  api.use('oauth', ['client', 'server']);
-  api.use('http', 'server');
+  api.use('oauth2@1.3.1', ['client', 'server']);
+  api.use('oauth@2.0.0', ['client', 'server']);
+  api.use('http@2.0.0', 'server');
   api.use('underscore', 'server');
   api.use('random', 'client');
   api.use('service-configuration', ['client', 'server']);
